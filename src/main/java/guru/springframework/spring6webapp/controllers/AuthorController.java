93468afa712c54e61df1588c6acc566942af2706
My Name <me@gm.com>
2025-01-23 15:44:52 +0100

    2.27.1-add-authors-assn : create a dedicated controller to interct with Author data

--- src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
@@ -1,2 +1,26 @@
-package guru.springframework.spring6webapp.controllers;public class AuthorController {
+package guru.springframework.spring6webapp.controllers;
+
+import guru.springframework.spring6webapp.services.AuthorService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorController(final AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/authors")
+    public String getAuthors(final Model model) {
+
+        model.addAttribute("authors", authorService.findAll());
+
+        return "authors";
+    }
+
 }
